ebef8c8728ded8c0de8cb3eb680a531e6dbb2c0a
Scott Molsness <smolsness@gmail.com>
2018-03-02 10:58:11 -0800

    updated trip show ejs with forms for each category, hiding other forms by default

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-   $('select').material_select();
+  $('.categories').hide();
+  $('select').material_select();
+
+  var categoryselect = $('#categoryselect').val();
+
+  switch (categoryselect) {
+    case 'Active Lifestyle':
+      $('#activeform').show();
+      break;
+    case 2:
+      $('#entertainmentform').show();
+      break;
+    case 3:
+      $('#beautyform').show();
+      break;
+    case 4:
+      $('#foodform').show();
+      break;
+    case 5:
+      $('#hotelsform').show();
+      break;
+    case 6:
+      $('#nightlifeform').show();
+      break;
+    case 7:
+      $('#restaurantsform').show();
+      break;
+    case 8:
+      $('#shoppingform').show();
+      break;
+    }
  });
